b1c3a8e210cc8d661f0ca0e9f9bc70d18d41ecfa
night-mareLuna <lunanight115@gmail.com>
2023-12-21 01:20:05 +0200

    create config folders/files on first launch

--- Mapping_Tools/Program.cs
+++ Mapping_Tools/Program.cs
@@ -1,23 +1,25 @@
 ﻿using Avalonia;
-using Avalonia.ReactiveUI;
+using Avalonia_Mapping_Tools.Models;
 using System;
 
 namespace Avalonia_Mapping_Tools;
 
-class Program
+public class Program
 {
+	public static readonly string configPath = AppDomain.CurrentDomain.BaseDirectory + "MappingToolsConfigs";
     // Initialization code. Don't use any Avalonia, third-party APIs or any
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args) 
+	{
+		BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+	}
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
             .UsePlatformDetect()
             .WithInterFont()
-            .LogToTrace()
-            .UseReactiveUI();
+            .LogToTrace();
 }
