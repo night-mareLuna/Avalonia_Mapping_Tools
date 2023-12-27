a7d58b691966db9fbf3e23b53938255f805f9cee
night-mareLuna <lunanight115@gmail.com>
2023-12-28 00:58:24 +0200

    separate config path if debug or release build

--- Mapping_Tools/Program.cs
+++ Mapping_Tools/Program.cs
@@ -5,8 +5,11 @@ namespace Avalonia_Mapping_Tools;
 
 public class Program
 {
+#if DEBUG
 	public static readonly string configPath = AppDomain.CurrentDomain.BaseDirectory + "MappingTools";
-	//public static readonly string configPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/Mapping Tools";
+#else
+	public static readonly string configPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/Mapping Tools";
+#endif
 
     // Initialization code. Don't use any Avalonia, third-party APIs or any
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
