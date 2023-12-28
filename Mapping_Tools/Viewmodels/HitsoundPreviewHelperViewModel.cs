4b28117d215460a736f4fc5b1a63fd6673410f7f
night-mareLuna <lunanight115@gmail.com>
2023-12-28 23:48:17 +0200

    add (most) bindings to hitsound preview helper

--- Mapping_Tools/Viewmodels/HitsoundPreviewHelperViewModel.cs
+++ Mapping_Tools/Viewmodels/HitsoundPreviewHelperViewModel.cs
@@ -1,9 +1,72 @@
+using Mapping_Tools.Classes;
+using Mapping_Tools.Classes.HitsoundStuff;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using MsBox.Avalonia;
+using MsBox.Avalonia.Enums;
+
 namespace Avalonia_Mapping_Tools.ViewModels;
 
 public partial class HitsoundPreviewHelperViewModel : ViewModelBase
 {
+	[ObservableProperty] private ObservableCollection<HitsoundZone> _Items;
+	[JsonIgnore] [ObservableProperty] private int _Progress = 0;
+	[JsonIgnore] [ObservableProperty] private bool? _IsAllItemsSelected;
 	public HitsoundPreviewHelperViewModel()
 	{
-
+		Items = [];
 	}
+
+	public void AddCommand()
+	{
+		Items.Add(new());
+	}
+
+	public void RemoveCommand()
+	{
+		try
+		{
+			Items.RemoveAll(o => o.IsSelected);
+		}
+		catch(Exception e)
+		{
+			e.Show();
+		}
+	}
+
+	public async void DeleteAllCommand()
+	{
+		var box = MessageBoxManager.GetMessageBoxStandard("",
+			"Are you sure you want to DELETE ALL hitsound zones and start from scratch?",
+			ButtonEnum.YesNo);
+		var result = await box.ShowAsync();
+		if(result == ButtonResult.Yes)
+		{
+			SelectAll(true, Items);
+			RemoveCommand();
+		}
+		else return;
+	}
+
+	public void CopyCommand()
+	{
+		try
+		{
+            int initialCount = Items.Count;
+            for (int i = 0; i < initialCount; i++) {
+                if (Items[i].IsSelected) {
+                    Items.Add(Items[i].Copy());
+                }
+            }
+        } catch (Exception ex) { ex.Show(); }
+	}
+
+	private static void SelectAll(bool select, IEnumerable<HitsoundZone> models) {
+        foreach (var model in models) {
+            model.IsSelected = select;
+        }
+    }
 }
