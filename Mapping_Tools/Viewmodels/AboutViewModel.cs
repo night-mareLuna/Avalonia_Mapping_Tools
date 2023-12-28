691e2fbe8e98e043f658cea0a82912f5457009f1
night-mareLuna <lunanight115@gmail.com>
2023-12-28 02:40:32 +0200

    add some form of about window

--- Mapping_Tools/Viewmodels/AboutViewModel.cs
+++ Mapping_Tools/Viewmodels/AboutViewModel.cs
@@ -2,8 +2,21 @@ namespace Avalonia_Mapping_Tools.ViewModels;
 
 public partial class AboutViewModel : ViewModelBase
 {
+	public string URLGitHub { get; } = "https://github.com/night-mareLuna/Avalonia_Mapping_Tools";
+	public string URLMappingTools { get; } = "https://github.com/OliBomby/Mapping_Tools";
+	public string URLAvalonia { get; } = "https://www.avaloniaui.net/";
 	public AboutViewModel()
 	{
 
+	}
+
+	public void OpenLink(string link)
+	{
+		var psi = new System.Diagnostics.ProcessStartInfo
+		{
+			UseShellExecute = true,
+			FileName = link
+		};
+		System.Diagnostics.Process.Start(psi);
 	}
 }
