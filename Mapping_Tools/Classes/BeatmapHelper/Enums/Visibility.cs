db8cdaf4cc30a923966f815caf5fe3128834f891
night-mareLuna <lunanight115@gmail.com>
2023-12-29 16:06:30 +0200

    uneeded right now

--- Mapping_Tools/Classes/BeatmapHelper/Enums/Visibility.cs
+++
@@ -1,7 +0,0 @@
-namespace Mapping_Tools.Classes.BeatmapHelper.Enums;
-public enum Visibility
-		{
-			Visible = 0,
-			Hidden = 1,
-			Collapsed = 2
-		}
--- Mapping_Tools/Components/ToFix/Domain/BooleanOrToVisibilityConverter.cs
+++ Mapping_Tools/Components/ToFix/Domain/BooleanOrToVisibilityConverter.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Globalization;
 using Avalonia.Data.Converters;
-using Mapping_Tools.Classes.BeatmapHelper.Enums;
 
 namespace Mapping_Tools.Components.Domain
 {
