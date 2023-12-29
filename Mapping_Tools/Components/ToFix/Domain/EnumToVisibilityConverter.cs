14ba42401e0881322184e4889d8eae2e1be427ef
night-mareLuna <lunanight115@gmail.com>
2023-12-29 16:06:30 +0200

    uneeded right now

--- Mapping_Tools/Components/ToFix/Domain/EnumToVisibilityConverter.cs
+++ Mapping_Tools/Components/ToFix/Domain/EnumToVisibilityConverter.cs
@@ -2,7 +2,6 @@
 using System.Globalization;
 using Avalonia.Data;
 using Avalonia.Data.Converters;
-using Mapping_Tools.Classes.BeatmapHelper.Enums;
 
 namespace Mapping_Tools.Components.Domain{
     /// <summary>
