8f37ca9a21d9045523c07a1f1fa933c2c575a1f2
night-mareLuna <lunanight115@gmail.com>
2024-01-02 16:08:08 +0200

    complete export dialog binding things

--- Mapping_Tools/Views/HitsoundStudio/HitsoundStudioExportDialog.axaml.cs
+++ Mapping_Tools/Views/HitsoundStudio/HitsoundStudioExportDialog.axaml.cs
@@ -1,10 +1,35 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia_Mapping_Tools.ViewModels;
+using Mapping_Tools.Classes.SystemTools;
 
 namespace Avalonia_Mapping_Tools.Views.HitsoundStudio;
 public partial class HitsoundStudioExportDialog : Window
 {
-	public HitsoundStudioExportDialog()
+	public HitsoundStudioViewModel Settings => (HitsoundStudioViewModel) DataContext!;
+	public bool result = false;
+	public HitsoundStudioExportDialog(HitsoundStudioViewModel settings)
 	{
+		DataContext = settings;
 		InitializeComponent();
 	}
+
+	private async void ExportFolderBrowseButton_OnClick(object sender, RoutedEventArgs e) {
+        string path = await IOHelper.FolderDialog();
+        if (!string.IsNullOrWhiteSpace(path)) {
+            ExportFolderBox.Text = path;
+            Settings.ExportFolder = path;
+        }
+    }
+
+	private void SetResult(object obj, RoutedEventArgs args)
+	{
+        result = (obj as Button)!.Name! switch
+        {
+            "Accept" => true,
+            "Cancel" => false,
+            _ => false
+        };
+		Close();
+    }
 }
