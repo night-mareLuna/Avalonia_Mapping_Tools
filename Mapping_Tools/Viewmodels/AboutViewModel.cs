6efde1df758add9e075b7a9533b9d7d26c3b618a
night-mareLuna <lunanight115@gmail.com>
2024-01-13 19:01:02 +0200

    bump version

--- Mapping_Tools/Viewmodels/AboutViewModel.cs
+++ Mapping_Tools/Viewmodels/AboutViewModel.cs
@@ -5,7 +5,7 @@ public partial class AboutViewModel : ViewModelBase
 	public string URLGitHub { get; } = "https://github.com/night-mareLuna/Avalonia_Mapping_Tools";
 	public string URLMappingTools { get; } = "https://github.com/OliBomby/Mapping_Tools";
 	public string URLAvalonia { get; } = "https://www.avaloniaui.net/";
-	public string Version { get; } = "v0.0.3";
+	public string Version { get; } = "v0.0.4";
 	public AboutViewModel()
 	{
 
