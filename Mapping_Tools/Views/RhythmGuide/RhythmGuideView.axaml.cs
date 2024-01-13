1eb6058eb9405094fecc8d942b9580739c749e9e
night-mareLuna <lunanight115@gmail.com>
2024-01-13 18:51:21 +0200

    (mostly) complete rhythm guide port

--- Mapping_Tools/Views/RhythmGuide/RhythmGuideView.axaml.cs
+++ Mapping_Tools/Views/RhythmGuide/RhythmGuideView.axaml.cs
@@ -1,12 +1,65 @@
-using Avalonia.Controls;
+using System.ComponentModel;
+using Avalonia.Interactivity;
 using Avalonia_Mapping_Tools.ViewModels;
+using Mapping_Tools.Classes.SystemTools;
+using Mapping_Tools.Classes.Tools;
+using Mapping_Tools.Views;
 
 namespace Avalonia_Mapping_Tools.Views;
-public partial class RhythmGuideView : UserControl
+public partial class RhythmGuideView : SingleRunMappingTool, ISavable<RhythmGuideViewModel>
 {
 	public RhythmGuideView()
 	{
 		DataContext = new RhythmGuideViewModel();
 		InitializeComponent();
+		Verbose = true;
 	}
+
+	public RhythmGuideViewModel ViewModel => (RhythmGuideViewModel) DataContext!;
+
+	public RhythmGuideViewModel GetSaveData()
+	{
+		return ViewModel;
+	}
+
+	private async void Start_Click(object obj, RoutedEventArgs args)
+	{
+		if(BackgroundWorker.IsBusy) return;
+
+		foreach (var fileToCopy in ViewModel.GuideGeneratorArgs.Paths)
+			await BackupManager.SaveMapBackup(fileToCopy);
+
+		BackgroundWorker.RunWorkerAsync(ViewModel.GuideGeneratorArgs);
+	}
+
+    protected override void BackgroundWorker_DoWork(object? sender, DoWorkEventArgs e)
+    {
+        var bgw = sender as BackgroundWorker;
+		e.Result = GenerateRhythmGuide((RhythmGuide.RhythmGuideGeneratorArgs) e.Argument!, bgw!, e);
+    }
+
+    protected override void BackgroundWorker_ProgressChanged(object? sender, ProgressChangedEventArgs e)
+    {
+        base.BackgroundWorker_ProgressChanged(sender, e);
+		RhythmGuideViewModel.UpdateProgress(Progress);
+    }
+
+    private static string GenerateRhythmGuide(RhythmGuide.RhythmGuideGeneratorArgs args, BackgroundWorker worker, DoWorkEventArgs _)
+	{
+		RhythmGuide.GenerateRhythmGuide(args);
+
+		if(worker != null && worker.WorkerReportsProgress)
+			worker.ReportProgress(100);
+
+		return args.ExportMode == RhythmGuide.ExportMode.NewMap ? "" : "Done!";
+	}
+
+    public void SetSaveData(RhythmGuideViewModel saveData)
+	{
+		DataContext = saveData;
+	}
+
+	public string AutoSavePath => Program.configPath + "/rhythmguideproject.json";
+
+	public string DefaultSaveFolder => Program.configPath + "/Rhythm Guide Projects";
 }
