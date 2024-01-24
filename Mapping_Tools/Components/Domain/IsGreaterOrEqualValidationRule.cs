4d55d6f6f86d7d6ae017efa88565130705bbea82
night-mareLuna <lunanight115@gmail.com>
2024-01-25 00:47:48 +0200

    greater than validation rule port

---
+++ Mapping_Tools/Components/Domain/IsGreaterOrEqualValidationRule.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mapping_Tools.Components.Domain;
+public sealed class GreaterThanOrEqualAttribute(double propertyName) : ValidationAttribute
+{
+    protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+    {
+		if(value is null) return new("Error");
+
+		return propertyName <= (double)value ?
+			ValidationResult.Success! : new($"Value needs to be greater than or equal {propertyName}!");
+    }
+}
