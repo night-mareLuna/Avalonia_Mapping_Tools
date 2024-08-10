3ee09ef13fa1cee3523831d84fbbcee7091fc0ef
night-mareLuna <lunanight115@protonmail.com>
2024-08-10 18:45:12 +0200

    credit l3lackShark for gosumemory

--- Mapping_Tools/Viewmodels/AboutViewModel.cs
+++ Mapping_Tools/Viewmodels/AboutViewModel.cs
@@ -5,6 +5,7 @@ public partial class AboutViewModel : ViewModelBase
 	public string URLGitHub { get; } = "https://github.com/night-mareLuna/Avalonia_Mapping_Tools";
 	public string URLMappingTools { get; } = "https://github.com/OliBomby/Mapping_Tools";
 	public string URLAvalonia { get; } = "https://www.avaloniaui.net/";
+	public string URLGosumemory { get; } = "https://github.com/l3lackShark/gosumemory";
 	public string Version { get; } = "v0.0.6";
 	public AboutViewModel()
 	{
